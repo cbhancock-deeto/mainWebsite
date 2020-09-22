1c49423ca895163df413a164c8eb47b1679529af
Codes <cblainehancock@gmail.com>
2020-09-22 16:52:41 -0600

    ci test ci test 2

--- src/components/Layout/WebsiteFront/WebsiteFront.js
+++ src/components/Layout/WebsiteFront/WebsiteFront.js
@@ -4,8 +4,7 @@ import './WebsiteFront.css';
 function WebsiteFront() {
     return (
         <div className='bio'>
-            <p>CI deployment test</p>
-            <h1 className='name-title'>cody hancock</h1>
+            <h1 className='name-title'>CI DEPLOYMENT TEST</h1>
             <p className='name-subtitle'>developer bio</p>
         </div>
     )
