32bfb9e313b8f7fa21af3c8772eb5524cfeec40f
Codes <cblainehancock@gmail.com>
2020-09-22 16:50:55 -0600

    connected netlify integration

--- src/components/Layout/WebsiteFront/WebsiteFront.js
+++ src/components/Layout/WebsiteFront/WebsiteFront.js
@@ -4,6 +4,7 @@ import './WebsiteFront.css';
 function WebsiteFront() {
     return (
         <div className='bio'>
+            <p>CI deployment test</p>
             <h1 className='name-title'>cody hancock</h1>
             <p className='name-subtitle'>developer bio</p>
         </div>
