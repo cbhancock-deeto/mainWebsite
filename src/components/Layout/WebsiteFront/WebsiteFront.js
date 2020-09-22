12f5d28ff5996eb5fe538e9d09bf75aafa68ea48
Codes <cblainehancock@gmail.com>
2020-09-22 16:58:08 -0600

    moved social media icons

--- src/components/Layout/WebsiteFront/WebsiteFront.js
+++ src/components/Layout/WebsiteFront/WebsiteFront.js
@@ -4,7 +4,7 @@ import './WebsiteFront.css';
 function WebsiteFront() {
     return (
         <div className='bio'>
-            <h1 className='name-title'>CI DEPLOYMENT TEST</h1>
+            <h1 className='name-title'>cody hancock</h1>
             <p className='name-subtitle'>developer bio</p>
         </div>
     )
