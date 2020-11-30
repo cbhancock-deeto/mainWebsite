fbd1fc3675aa73c4452c99df626a6291076cc84b
cody <cblainehancock@gmail.com>
2020-11-30 14:54:52 -0700

    added Pun API to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,74 +1,68 @@
 import React from "react";
-import {
-  Grid,
-  Typography
-} from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import Toolbar from "../Layout/Toolbar/Toolbar";
 import projects from "./Projects";
-import ProjectCard from './ProjectCard';
+import ProjectCard from "./ProjectCard";
 
-const useStyles = makeStyles((theme) => ({
-  portfolioBackground1: {
-    backgroundColor: `${theme.palette.primary.main}`,
-  },
-  portfolioBackground2: {
-    backgroundColor: `${theme.palette.primary.main}`,
-    height: "100vh",
-  },
-  portfolioHeading: {
-    fontFamily: "Montserrat, sans-serif",
-    fontWeight: "200",
-    fontSize: "2rem",
-    textAlign: "center",
-    paddingBottom: 50,
-    paddingTop: 70,
-    color: `${theme.palette.secondary.main}`,
-    [theme.breakpoints.down("xs")]: {
-      fontWeight: "500",
-      textAlign: "center",
-      paddingBottom: 18,
-    },
-  },
+const useStyles = makeStyles(theme => ({
+	portfolioBackground1: {
+		backgroundColor: `${theme.palette.primary.main}`,
+	},
+	portfolioBackground2: {
+		backgroundColor: `${theme.palette.primary.main}`,
+		height: "100vh",
+	},
+	portfolioHeading: {
+		fontFamily: "Montserrat, sans-serif",
+		fontWeight: "200",
+		fontSize: "2rem",
+		textAlign: "center",
+		paddingBottom: 50,
+		paddingTop: 70,
+		color: `${theme.palette.secondary.main}`,
+		[theme.breakpoints.down("xs")]: {
+			fontWeight: "500",
+			textAlign: "center",
+			paddingBottom: 18,
+		},
+	},
 }));
 
 function Portfolio() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const projectList = projects.map((p) => (
-    <Grid item key={p.key}>
-      <ProjectCard
-        
-        url={p.url}
-        description={p.description}
-        title={p.title}
-        img={p.img}
-        github={p.github}
-      />
-    </Grid>
-  ));
+	const projectList = projects.map(p => (
+		<Grid item key={p.key}>
+			<ProjectCard
+				url={p.url}
+				description={p.description}
+				title={p.title}
+				img={p.img}
+				github={p.github}
+			/>
+		</Grid>
+	));
 
-  return (
-    <div className={classes.portfolioBackground2}>
-      <Toolbar />
-      <Grid
-        container
-        className={classes.portfolioBackground1}
-        justify="center"
-        alignItems="center"
-        spacing={3}
-      >
-        <Grid xs={12} item>
-          <Typography className={classes.portfolioHeading}>
-            Portfolio
-          </Typography>
-        </Grid>
+	return (
+		<div className={classes.portfolioBackground2}>
+			<Toolbar />
+			<Grid
+				container
+				className={classes.portfolioBackground1}
+				justify='center'
+				alignItems='center'
+				spacing={3}>
+				<Grid xs={12} item>
+					<Typography className={classes.portfolioHeading}>
+						Portfolio
+					</Typography>
+				</Grid>
 
-        {projectList}
-      </Grid>
-    </div>
-  );
+				{projectList}
+			</Grid>
+		</div>
+	);
 }
-
 
 export default Portfolio;
--- src/components/Portfolio/Projects.js
+++ src/components/Portfolio/Projects.js
@@ -1,23 +1,37 @@
-import tetrisImg from '../../assets/images/tetristhumbnail.PNG';
-import DALimg from '../../assets/images/deliberateAbundanceThumbnail.PNG';
+import tetrisImg from "../../assets/images/tetristhumbnail.PNG";
+import DALimg from "../../assets/images/deliberateAbundanceThumbnail.PNG";
+import punImg from "../../assets/images/pun.png";
 
 var Projects = [
-    {
-        key: 2,
-        url: '//deliberate-abundance-website.netlify.app',
-        github: '//github.com/cbhancock-deeto/deliberate-abundance-website',
-        img: DALimg,
-        title: 'Deliberate Abundance Website',
-        description: 'A website I was commissioned to make. This website utilizes React\'s Material-UI library to create a sleek and efficient end-user experience.',
-    },
-    {
-        key: 1,
-        url: '//cbhancock-deeto.github.io/tetrisGame',
-        github: '//github.com/cbhancock-deeto/tetrisGame',
-        img: tetrisImg,
-        title: 'JavaScript Tetris',
-        description: 'A project made in JavaScript, HTML, and CSS with no additional libraries or frameworks.  The purpose of this project was to familiarize myself with fundamental JavaScript principles.',
-    },
-]
+	{
+		key: 3,
+		url: "https://flamboyant-curran-acf969.netlify.app/",
+		github:
+			"https://github.com/cbhancock-deeto/pun-front-end",
+		img: punImg,
+		title: "The Punny API",
+		description:
+			"A simple front-end which pings an API that I created stocked with more that 50 jokes.  The API uses a Node.js backend that connects to a MongoDB database through Mongoose.",
+	},
+	{
+		key: 2,
+		url: "//deliberate-abundance-website.netlify.app",
+		github:
+			"//github.com/cbhancock-deeto/deliberate-abundance-website",
+		img: DALimg,
+		title: "Deliberate Abundance Website",
+		description:
+			"A website I was commissioned to make. This website utilizes React's Material-UI library to create a sleek and efficient end-user experience.",
+	},
+	{
+		key: 1,
+		url: "//cbhancock-deeto.github.io/tetrisGame",
+		github: "//github.com/cbhancock-deeto/tetrisGame",
+		img: tetrisImg,
+		title: "JavaScript Tetris",
+		description:
+			"A project made in JavaScript, HTML, and CSS with no additional libraries or frameworks.  The purpose of this project was to familiarize myself with fundamental JavaScript principles.",
+	},
+];
 
-export default Projects
+export default Projects;
